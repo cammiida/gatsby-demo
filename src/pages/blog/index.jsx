c440ec11c42797b2fe85355ad2e259212602bb81
Camilla <camillamdalan@gmail.com>
2021-11-02 14:21:10 +0100

    Finished Gatsby Tutorial Part 6

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,19 +1,16 @@
-import { graphql, useStaticQuery } from "gatsby";
-import { MDXRenderer } from "gatsby-plugin-mdx";
+import { Link, graphql } from "gatsby";
 import React from "react";
-import Layout from "../components/layout";
+import Layout from "../../components/layout";
 
-const BlogPage = ({}) => {
-  const data = useStaticQuery(query);
-  console.log("blogposts: ", data);
-
+const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
       {data.allMdx.nodes.map((node) => (
         <article key={node.id}>
-          <h2>{node.frontmatter.title}</h2>
+          <h2>
+            <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
+          </h2>
           <p>Posted: {node.frontmatter.date}</p>
-          <MDXRenderer>{node.body}</MDXRenderer>
         </article>
       ))}
     </Layout>
@@ -31,7 +28,7 @@ export const query = graphql`
           title
         }
         id
-        body
+        slug
       }
     }
   }
---
+++ src/pages/blog/{mdx.slug}.jsx
@@ -0,0 +1,28 @@
+import { graphql } from "gatsby";
+import { MDXRenderer } from "gatsby-plugin-mdx";
+import React from "react";
+import Layout from "../../components/layout";
+
+const BlogPost = (props) => {
+  const { data } = props;
+  return (
+    <Layout pageTitle="Super Cool Blog Posts">
+      <p>{data.mdx.frontmatter.date}</p>
+      <MDXRenderer>{data.mdx.body}</MDXRenderer>
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  query ($id: String) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        date(formatString: "MMMM D, YYYY")
+      }
+      body
+    }
+  }
+`;
+
+export default BlogPost;
